c141a532bfaac940ad791de5e8bcd65390820863
Ondrej Balas <ondrej@ondrejbalas.com>
2014-11-08 22:10:22 -0800

    Now drawing a grid of randomly colored squares

--- js/app.js
+++ js/app.js
@@ -1,16 +1,48 @@
+var app = {};
+app.config = {
+    squareSize: 32,
+    mapWidth: 18,
+    mapHeight: 12
+};
+var xx = [[]]; xx[0][1] = 5;
+console.log(xx[0][1]);
 stage = new createjs.Stage("firstCanvas");
 
-circle = new createjs.Shape();
-circle.graphics.beginFill("red").drawCircle(0, 0, 40);
-//Set position of Shape instance.
-circle.x = circle.y = 200;
-//Add Shape instance to stage display list.
-stage.addChild(circle);
-//Update stage will render next frame
-stage.update();
+var Square = function(xPos, yPos) {
+    this.x = xPos;
+    this.y = yPos;
+    this.color = Math.random() > 0.5 ? "red" : "blue";
+    this.shape = new createjs.  Shape();
+    this.shape.graphics.beginFill(this.color).drawRect(0, 0, app.config.squareSize, app.config.squareSize);
+    stage.addChild(this.shape);
+    this.draw = function() {
+        this.shape.x = this.x * app.config.squareSize;
+        this.shape.y = this.y * app.config.squareSize;
+    };
+};
 
-circle.addEventListener("click", handleClick);
-function handleClick(event){
-    circle.x = circle.y = circle.y - 20;
-    stage.update();
-}
+var Map = function(width, height) {
+    this.width = width;
+    this.height = height;
+    this.squares = [[]];
+
+    for(i = 0; i < width; i++) {
+        this.squares[i] = [];
+        for (j = 0; j < height; j++) {
+            this.squares[i][j] = new Square(i, j);
+        }
+    }
+
+    this.draw = function() {
+        for(i = 0; i < width; i++) {
+            for (j = 0; j < height; j++) {
+                this.squares[i][j].draw();
+            }
+        }
+    };
+};
+
+var map = new Map(app.config.mapWidth, app.config.mapHeight);
+map.draw();
+
+stage.update();
